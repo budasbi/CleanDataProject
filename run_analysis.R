3e4fa326c6505bbc0fee14106be7c51fb61c5b07
Oscar Sierra Escamilla <oscar.sierra@infotrack.com.co>
2020-06-25 00:26:50 -0500

    Script for tidying data, and Summarize the data with the mean in every variable, grouped by Activity and idSubject

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,83 @@
-#1
+
 #Merges the training and the test sets to create one data set.
+urlInitialFiles<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+DestPath<-paste0(getwd(),"/InitialFile.zip")
+download.file(url =  urlInitialFiles, destfile = DestPath)
+listFiles<-unzip(DestPath, list = TRUE)
+#1.2 Exract Files
+unzip(DestPath)
 
-#2
-#Extracts only the measurements on the mean and standard deviation for each measurement.
+trainlist<-list.files(paste0(getwd(),"/UCI HAR Dataset","/train"),recursive = TRUE)
 
-#3
-#Uses descriptive activity names to name the activities in the data set
+listfiles<-paste0(getwd(),"/UCI HAR Dataset","/train/",trainlist)
+resultdirectory<-paste0(getwd(),"/UCI HAR Dataset","/result/")
+dir.create(resultdirectory)
+##Merge Files
+  for (trainfile in listfiles) {
+    
+    Data1Archivo<-read.table(trainfile, header = FALSE)
+    
+    archivo2<-gsub("_train","_test",gsub("/train/", "/test/",trainfile))
+    Data2Archivo<-read.table(archivo2, header = FALSE)
+    head(Data1Archivo, 2)
+      
+    resultdata<-rbind(Data1Archivo, Data2Archivo)
+    
+    splitnamesList<-strsplit(trainfile,"/")[length(strsplit(trainfile,"/"))]    
+    DestFile<-gsub("_train", "",splitnamesList[[1]][length(splitnamesList[[1]])])
+    
+    write.csv(resultdata,paste0(resultdirectory, DestFile))
+    
+    
+  } 
+##Cleaning Variables
+rm(listfiles, DestFile, splitnamesList, Data1Archivo, Data2Archivo, resultdata, trainfile, archivo2,trainlist )
+#Agregar Actividad y(Id actividad) con X cBind
+DataX<-read.table(paste0(resultdirectory, "X.txt"), header = FALSE, sep = ",", skip = 1)#Se carga el Archivo de las Observaciones
+encabezados<-read.table(paste0(getwd(),"/UCI HAR Dataset/features.txt"), header = FALSE, sep = " ")##Se cargan Los encabezados
+names(DataX)<-c("Num", encabezados[,2]) ##Se añaden encabezados
+Subject<-read.table(paste0(resultdirectory, "subject.txt"), header = FALSE, sep = ",", skip = 1)##Se Cargan los Sujetos
+subjects<-Subject[,2]##Se selecciona la columna con el Sujeto ID
+DataX<-cbind(subjects,DataX)##Se añade como primera columna, la columna de los sujetos id
 
-#4
-#Appropriately labels the data set with descriptive variable names.
 
-#5
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+DataActivity<-read.table(paste0(resultdirectory, "y.txt"), header = FALSE, sep = ",", skip = 1)##Se Cargan las Actividades
+Activitylabels<-read.table(paste0(getwd(),"/UCI HAR Dataset/activity_labels.txt"), header = FALSE, sep = "")##Se Cargan las Actividades
+ActivityWithLabels<-merge(DataActivity,Activitylabels, by.x = "V2", by.y = "V1")
+select(ActivityWithLabels, V1, V2.y)%>%arrange(V1)
+ActivitiesOrdered<-select(ActivityWithLabels, V1, V2.y)%>%arrange(V1)
+ActivitiesOrdered<-ActivitiesOrdered[,2]
+DataX<-cbind(ActivitiesOrdered,DataX)
+
+
+names(DataX)
+length(grep("([Mm]ean|[Ss]td)|subjects|ActivitiesOrdered", names(DataX)))
+TidyDataX<-DataX[,grep("([Mm]ean|[Ss]td)|subjects|ActivitiesOrdered", names(DataX))]
+length(names(TidyDataX))
+
+###############################TidyData for X
+tidydatadftb<-tbl_df(TidyDataX)
+
+############Gruped by Subject and Activity and getss the mean of all Columns
+groupedByActivityAndSubj<-group_by(TidyDataX,ActivitiesOrdered, subjects )%>%summarise_all(list(mean))
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
